58bdd977f6105f6f002028e63a6d8ed8dfd2889f
aLittleFat <1478527245@qq.com>
2020-03-04 16:40:05 +0800

    添加 申报个人竞赛的功能

--- src/components/MyMenu.vue
+++ src/components/MyMenu.vue
@@ -1,50 +1,183 @@
 <template>
-  <Menu theme="light" width="auto" :open-names="['1']">
-      <Submenu name="1">
-          <template slot="title">
-              <Icon type="ios-navigate"></Icon>
-              审核
-          </template>
-          <MenuItem :to="{name:'RegisterVerify'}">注册审核</MenuItem>
-      </Submenu>
-      <Submenu name="2">
-          <template slot="title">
-              <Icon type="ios-navigate"></Icon>
-              个人
-          </template>
-          <MenuItem :to="{name:'Overview'}">概览</MenuItem>
-          <MenuItem :to="{name:'MyAcRecord'}">刷题记录</MenuItem>
-          <MenuItem :to="{name:'MyContest'}">比赛记录</MenuItem>
-      </Submenu>
-      <Submenu name="3">
-          <template slot="title">
-              <Icon type="ios-keypad"></Icon>
-              设置
-          </template>
-          <MenuItem :to="{name:'Information'}">个人信息</MenuItem>
-          <MenuItem :to="{name:'OjAccount'}">OJ账号</MenuItem>
-      </Submenu>
-      <Submenu name="4">
-          <template slot="title">
-              <Icon type="ios-analytics"></Icon>
-              排行榜
-          </template>
-          <MenuItem :to="{name:'PersonalAcRank'}">个人刷题榜</MenuItem>
-          <MenuItem>Option 2</MenuItem>
-      </Submenu>
-      <Submenu name="5">
-          <template slot="title">
-              <Icon type="ios-analytics"></Icon>
-              管理
-          </template>
-          <MenuItem :to="{name:'SeasonListAdmin'}">赛季管理</MenuItem>
-      </Submenu>
+  <Menu ref="menu" theme="light" width="auto" :open-names="['1']" @on-select="selectMenu()">
+    <MenuGroup title="审核">
+        <MenuItem name="RegisterVerify" :to="{name:'RegisterVerify'}">注册审核</MenuItem>
+    </MenuGroup>
+    <MenuGroup title="个人">
+        <MenuItem name="Overview" :to="{name:'Overview'}">概览</MenuItem>
+        <MenuItem name="MyAcRecord" :to="{name:'MyAcRecord'}">刷题记录</MenuItem>
+        <MenuItem name="MyContest" :to="{name:'MyContest'}">比赛记录</MenuItem>
+        <MenuItem name="addPersonalContestRecord" >申报个人比赛</MenuItem>
+        <Modal v-model="add_personal_contest_modal" width="360">
+          <p slot="header" style="text-align:center">
+            <span>申报个人竞赛</span>
+          </p>
+          <Form :model="formItem" :label-width="100" :rules="rule" ref="addPersonalContestRecordForm">
+            <FormItem label="OJ" prop="oj">
+              <Select v-model="formItem.oj">
+                <Option v-for="item in ojList" :value="item.value" :key="item.value">{{item.label}}</Option>
+              </Select>
+            </FormItem>
+            <FormItem label="竞赛ID" prop="cId">
+              <Input v-model="formItem.cId" placeholder="请输入竞赛ID"></Input>
+            </FormItem>
+            <FormItem v-if="formItem.needPassword && formItem.oj !== 'HDU'" label="竞赛密码" prop="password">
+              <Input type="password" v-model="formItem.password" placeholder="请输入参赛密码"></Input>
+            </FormItem>
+            <FormItem label="参赛账号" prop="account">
+              <Input v-model="formItem.account" placeholder="请输入参赛账号"></Input>
+            </FormItem>
+            <FormItem v-if="formItem.needPassword && formItem.oj === 'HDU'" label="登录密码" prop="password">
+              <Input type="password" v-model="formItem.password" placeholder="请输入登录密码"></Input>
+            </FormItem>
+          </Form>
+          <div slot="footer">
+            <Button type="success" :loading="add_loading" @click="addPersonalContest()">添加</Button>
+          </div>
+        </Modal>
+    </MenuGroup>
+    <MenuGroup title="设置">
+        <MenuItem name="Information" :to="{name:'Information'}">个人信息</MenuItem>
+        <MenuItem name="OjAccount" :to="{name:'OjAccount'}">OJ账号</MenuItem>
+    </MenuGroup>
+    <MenuGroup title="排行榜">
+        <MenuItem name="PersonalAcRank" :to="{name:'PersonalAcRank'}">个人刷题榜</MenuItem>
+    </MenuGroup>
+    <MenuGroup title="管理">
+        <MenuItem name="SeasonListAdmin" :to="{name:'SeasonListAdmin'}">赛季管理</MenuItem>
+    </MenuGroup>
   </Menu>
 </template>
 
 <script>
   export default {
-    name: 'MyMenu'
+    name: 'MyMenu',
+    data () {
+      return {
+        add_personal_contest_modal: false,
+        add_loading: false,
+        formItem: {
+          oj: '',
+          account: '',
+          password: '',
+          cId: '',
+          needPassword: false
+        },
+        rule: {
+          oj: [{
+            required: true,
+            tigger: 'blur'
+          }],
+          cId: [{
+            required: true,
+            tigger: 'blur',
+            message: '请输入竞赛ID'
+          }],
+          account: [{
+            required: true,
+            tigger: 'blur',
+            message: '请输入参赛账号'
+          }],
+          password: [{
+            required: true,
+            tigger: 'blur',
+            message: '请输入密码'
+          }]
+        },
+        ojList: [
+          {
+            value: 'VJ',
+            label: 'VJ'
+          },
+          {
+            value: 'HDU',
+            label: 'HDU'
+          },
+          {
+            value: 'CodeForces',
+            label: 'CodeForces'
+          },
+          {
+            value: '计蒜客',
+            label: '计蒜客'
+          },
+          {
+            value: '牛客',
+            label: '牛客'
+          }
+        ]
+      }
+    },
+    created: function () {
+       //
+    },
+    methods: {
+      addPersonalContest () {
+        let that = this
+        that.$refs['addPersonalContestRecordForm'].validate((valid) => {
+          if (valid) {
+            that.add_loading = true
+            that.$http
+              .put('/api/personalContestRecord', {
+                ojName: that.formItem.oj,
+                cId: that.formItem.cId,
+                account: that.formItem.account,
+                password: that.formItem.password
+              })
+              .then(res => {
+                that.add_loading = false
+                if (res.data.status === 0) {
+                  that.$Message.success('添加成功')
+                  that.add_personal_contest_modal = false
+                } else {
+                  that.$Message.error(res.data.msg)
+                  if (res.data.msg === '需要密码') {
+                    that.formItem.needPassword = true
+                  }
+                  if (res.data.msg === '该比赛需要登录') {
+                    that.formItem.needPassword = true
+                  }
+                }
+              })
+          } else {
+            that.$Message.error('请正确填写表单')
+          }
+        })
+      },
+      selectMenu () {
+        let that = this
+        console.log(that.$refs['menu'])
+        if (that.$refs['menu'].currentActiveName === 'addPersonalContestRecord') {
+          that.formItem.needPassword = false
+          that.$refs['addPersonalContestRecordForm'].resetFields()
+          that.add_personal_contest_modal = true
+        }
+      }
+    },
+    computed: {
+      oj () {
+        return this.formItem.oj
+      }
+    },
+    watch: {
+      oj: function () {
+        let that = this
+        that.formItem.needPassword = false
+        that.formItem.password = ''
+        that.formItem.cId = ''
+        that.$http
+          .get('/api/ojAccount', {
+            params: {
+              ojName: that.formItem.oj
+            }
+          })
+          .then(res => {
+            if (res.data.status === 0) {
+              that.formItem.account = res.data.data
+            }
+          })
+      }
+    }
   }
 </script>
 
